e8a1e28262692bfe55ec861c8d40079ddc5e8258
Karvor Dope <65592198+Karvor@users.noreply.github.com>
2020-06-09 08:41:07 +0300

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -44,7 +44,7 @@ async def _(event):
 
     me = borg.me
     userid = borg.uid
-    packname = f"{userid}'s @UniBorg Pack"
+    packname = f"@KarvorTM's pack"
     packshortname = f"Uni_Borg_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
