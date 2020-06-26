c4665b8fd62edd9ced3a9fcaf2e26a5277d85ea0
KenXer <65592198+KenXer@users.noreply.github.com>
2020-06-26 11:49:09 +0300

    Update account_profile.py

--- stdplugins/account_profile.py
+++ stdplugins/account_profile.py
@@ -73,3 +73,70 @@ async def _(event):
         os.remove(photo)
     except Exception as e:  # pylint:disable=C0103,W0703
         logger.warn(str(e))  # pylint:disable=E0602
+
+
+borg.on(admin_cmd(pattern="kbio (.*)", allow_sudo=True))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    bio = event.pattern_match.group(1)
+    try:
+        await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+            about=bio
+        ))
+        await event.reply("Succesfully changed my profile bio")
+    except Exception as e:  # pylint:disable=C0103,W0703
+        await event.edit(str(e))
+
+
+@borg.on(admin_cmd(pattern="kname ((.|\n)*)", allow_sudo=True))  # pylint:disable=E0602,W0703
+async def _(event):
+    if event.fwd_from:
+        return
+    names = event.pattern_match.group(1)
+    first_name = names
+    last_name = ""
+    if  "\\n" in names:
+        first_name, last_name = names.split("\\n", 1)
+    try:
+        await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+            first_name=first_name,
+            last_name=last_name
+        ))
+        await event.reply("My name was changed successfully")
+    except Exception as e:  # pylint:disable=C0103,W0703
+        await event.edit(str(e))
+
+
+@borg.on(admin_cmd(pattern="kpic", allow_sudo=True))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_message = await event.get_reply_message()
+    await event.reply("Downloading Profile Picture to my local ...")
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):  # pylint:disable=E0602
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)  # pylint:disable=E0602
+    photo = None
+    try:
+        photo = await borg.download_media(  # pylint:disable=E0602
+            reply_message,
+            Config.TMP_DOWNLOAD_DIRECTORY  # pylint:disable=E0602
+        )
+    except Exception as e:  # pylint:disable=C0103,W0703
+        await event.edit(str(e))
+    else:
+        if photo:
+            await event.reply("now, Uploading to @Telegram ...")
+            file = await borg.upload_file(photo)  # pylint:disable=E0602
+            try:
+                await borg(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
+                    file
+                ))
+            except Exception as e:  # pylint:disable=C0103,W0703
+                await event.edit(str(e))
+            else:
+                await event.reply("My profile picture was succesfully changed")
+    try:
+        os.remove(photo)
+    except Exception as e:  # pylint:disable=C0103,W0703
+        logger.warn(str(e))  # pylint:disable=E0602
