9b208ca17507752c5aa821725a809c720282df25
Real Ninja <65592198+KenXer@users.noreply.github.com>
2020-07-26 15:44:14 +0300

    Update whois.py

--- stdplugins/whois.py
+++ stdplugins/whois.py
@@ -9,7 +9,7 @@ from telethon.utils import get_input_location
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="w ?(.*)"))
+@borg.on(admin_cmd(pattern="w ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
