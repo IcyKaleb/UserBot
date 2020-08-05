415d4c18b338c3e47a3b88b450f0eb1ac7770530
Real Ninja <65592198+Xaleb1370@users.noreply.github.com>
2020-08-05 21:58:20 +0300

    Updated

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -15,7 +15,7 @@ from telethon import events, functions, __version__
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="on ?(.*)", allow_sudo=True))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="jaleb ?(.*)", allow_sudo=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
